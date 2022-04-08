855967eaa00090eae2ae156e447df01970f55c86
arnabmitra <arnabmitra.mitra@gmail.com>
2022-04-08 15:49:58 -0600

    more changes

--- simple_send.sh
+++ simple_send.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
-
+x=1
 PROVENANCE_DEV_DIR=~/provenance
 ${PROVENANCE_DEV_DIR}/build/provenanced -t --home ${PROVENANCE_DEV_DIR}/build/node0 keys add ownermarker --recover --hd-path "44'/1'/0'/0/0'" --keyring-backend test < ./mnemonics/ownermarker.txt
-COMMON_TX_FLAGS="--gas auto --gas-prices 1905nhash --gas-adjustment 2 --chain-id chain-local --keyring-backend test --yes -o json"
+COMMON_TX_FLAGS="--gas auto --gas-prices 190nhash --gas-adjustment 2 --chain-id chain-local --keyring-backend test --yes -o json"
+
+while [ $x -le 5 ]
+do
+
 
  ${PROVENANCE_DEV_DIR}/build/provenanced tx bank send \
   $(${PROVENANCE_DEV_DIR}/build/provenanced keys show -a node0 --home ${PROVENANCE_DEV_DIR}/build/node0 --keyring-backend test --testnet) \
@@ -11,5 +14,7 @@ COMMON_TX_FLAGS="--gas auto --gas-prices 1905nhash --gas-adjustment 2 --chain-id
   1000000000000nhash \
   --from node0 \
   --home ${PROVENANCE_DEV_DIR}/build/node0 \
-  --keyring-backend test --chain-id chain-local --fees 382199010nhash --broadcast-mode block --yes \
+  --keyring-backend test --chain-id chain-local --gas 40000000000 --gas-prices 1905nhash  --yes \
   --testnet -o json | jq
+  x=$(( x+1 ))
+done
