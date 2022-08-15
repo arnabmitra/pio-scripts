56924bea24d2884adb1d16bfcd3c73c75b175adf
arnabmitra <arnabmitra.mitra@gmail.com>
2022-08-15 15:04:49 -0600

    gov_change_param_new_voting_action.sh

---
+++ gov_change_param_new_voting_action.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# I'm using a provenance "run" instance for these tests
+# provenance setup, clean project, make, adjust the voting period in the config to test msg based fee proposals:
+# 'make clean ; make build; make run-config; cat ./build/node0/config/genesis.json | jq '\'' .app_state.gov.voting_params.voting_period="20s" '\'' | tee ./build/node0/config/genesis.json; cat ./build/node0/config/genesis.json'
+
+PROVENANCE_DEV_DIR=~/provenance
+
+COMMON_TX_FLAGS="--gas auto --gas-prices 1905nhash --gas-adjustment 2 --chain-id chain-local --keyring-backend test --yes -o json"
+
+######################################## SETUP FOR GOV PROPOSAL ##############################################
+
+GOV_PROP=$(${PROVENANCE_DEV_DIR}/build/provenanced tx gov submit-proposal param-change ./param-change-proposal.json \
+  --from node0 \
+  --home ${PROVENANCE_DEV_DIR}/build/node0 \
+  --chain-id chain-local \
+  --keyring-backend test \
+  --fees 1000000000nhash \
+  --gas auto \
+  --gas-adjustment 2 \
+  --broadcast-mode block \
+  --yes \
+  --testnet -o json | jq '.logs[ ] .events[] | select(.type=="submit_proposal") .attributes[]| select(.key=="proposal_id") .value')
+GOV_PROP=$(sed -e 's/^"//' -e 's/"$//' <<<"$GOV_PROP")
+# shellcheck disable=SC2086
+printf $GOV_PROP
+
+${PROVENANCE_DEV_DIR}/build/provenanced -t tx gov weighted-vote $GOV_PROP yes=1.0 \
+  --from node0 \
+  --home ${PROVENANCE_DEV_DIR}/build/node0 \
+  --chain-id chain-local \
+  --keyring-backend test \
+  --gas-prices 1905nhash \
+  --gas 150000 \
+  --broadcast-mode block \
+  --yes \
+  --testnet
+
+${PROVENANCE_DEV_DIR}/build/provenanced -t tx gov weighted-vote $GOV_PROP yes=1.0 \
+  --from node1 \
+  --home ${PROVENANCE_DEV_DIR}/build/node1 \
+  --chain-id chain-local \
+  --keyring-backend test \
+  --gas-prices 1905nhash \
+  --gas 150000 \
+  --broadcast-mode block \
+  --yes \
+  --testnet
+
+${PROVENANCE_DEV_DIR}/build/provenanced -t tx gov weighted-vote $GOV_PROP yes=1.0 \
+  --from node2 \
+  --home ${PROVENANCE_DEV_DIR}/build/node2 \
+  --chain-id chain-local \
+  --keyring-backend test \
+  --gas-prices 1905nhash \
+  --gas 150000 \
+  --broadcast-mode block \
+  --yes \
+  --testnet
+
+${PROVENANCE_DEV_DIR}/build/provenanced -t tx gov weighted-vote "$GOV_PROP" yes=1.0 \
+  --from node3 \
+  --home ${PROVENANCE_DEV_DIR}/build/node3 \
+  --chain-id chain-local \
+  --keyring-backend test \
+  --gas-prices 1905nhash \
+  --gas 150000 \
+  --broadcast-mode block \
+  --yes \
+  --testnet
