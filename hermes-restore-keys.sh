6a9b8a067cc1d8a8abf1b5de368f2a3d20186e4d
arnabmitra <arnabmitra.mitra@gmail.com>
2023-02-03 11:09:37 -0700

    updating hermes key restore script.

--- hermes-restore-keys.sh
+++ hermes-restore-keys.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
-
-
-### Sleep is needed otherwise the relayer crashes when trying to init
-sleep 1
+# providing path to match golang relayer
 hermes --config ~/.hermes/config.toml keys add --chain testing --key-name testing --mnemonic-file ./mnemonics/relayer.txt --hd-path="m/44'/118'/0'/0/0"
-sleep 5
 
 hermes --config ~/.hermes/config.toml keys add --chain testing2 --key-name testing2 --mnemonic-file ./mnemonics/relayer.txt --hd-path="m/44'/118'/0'/0/0"
-sleep 5
